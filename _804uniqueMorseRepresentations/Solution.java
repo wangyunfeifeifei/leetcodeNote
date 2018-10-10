260ac3074f94131fd9c9cdd9842494223e11905b
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-10-10 08:58:13 +0800

    添加804唯一的摩尔斯密码题解

---
+++ _804uniqueMorseRepresentations/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+            String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    
+    Set<String> set = new TreeSet<>();
+    
+    for(String word : words) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i< word.length(); i++) {
+            res.append(codes[word.charAt(i) - 'a']);
+        }
+        set.add(res.toString());
+    }
+    return set.size();
+    }
+}
