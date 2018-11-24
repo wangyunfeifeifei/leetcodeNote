caecbbb456dde2fa38869c35e32b85e05b02e9ef
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-11-24 17:36:12 +0800

    新增278题第一个错误版本题解

---
+++ _278FirstBadVersion/Solution.java
@@ -0,0 +1,16 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        int mid = l + (r-l)/2;
+        while(l < r) {
+            if(isBadVersion(mid)) r = mid;
+            else l = mid + 1;
+            mid = l + (r-l)/2;
+        }
+        return l;
+    }
+}
