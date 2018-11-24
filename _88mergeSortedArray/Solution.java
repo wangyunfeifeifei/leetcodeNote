616811e5bd3d5679b6f334c8ac6b43b97a5a6523
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-11-24 17:03:17 +0800

    新增88题合并有序数组题解

---
+++ _88mergeSortedArray/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+        while(index >= 0) {
+            if(i < 0) {
+                nums1[index--] = nums2[j--];
+            }else if (j < 0){
+                nums1[index--] = nums1[i--];
+            } else if(nums1[i] > nums2[j]) {
+                nums1[index--] = nums1[i--];
+            } else {
+                nums1[index--] = nums2[j--];
+            }
+        }
+    }
+}
